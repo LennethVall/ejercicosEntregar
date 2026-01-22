6eb3c31babdf48e8be644c5b65efe0a3c732dd31
LennethVall <sindelenia@gmail.com>
2026-01-22 19:30:02 +0100

    Update Main.java
    un poco mas

--- gimnasioTartanga/main/Main.java
+++ gimnasioTartanga/main/Main.java
@@ -1,13 +1,18 @@
 package main;
 
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import clases.Cargo;
+import clases.Cliente;
 import clases.DNIException;
 import clases.Trabajador;
 import clases.Utilidades;
@@ -37,12 +42,12 @@ public class Main {
 
                 case 1:
                     // Ver trabajadores (directamente desde fichero)
-                    verTrabajadores();
+                    verTrabajadores(refFichero);
                     break;
 
                 case 2:
                     // Ver clientes ordenados por nombre
-                    verClientesOrdenados();
+                    verClientesOrdenados(refFichero);
                     break;
 
                 case 3:
@@ -81,9 +86,9 @@ public class Main {
 			boolean DNICorrecto = false;
 			String DNI="";
 			String nombre="";
-			Stringh contraseña=""
-			Strinh usuario="";
-			Cargo cargo;
+			String contraseña="";
+			String usuario="";
+			Cargo cargo=null;
 			while (!DNICorrecto) {
 				try {
 
@@ -101,15 +106,17 @@ public class Main {
 						usuario = Utilidades.introducirCadena("introduce nombre de usuario: ");
 						contraseña = Utilidades.introducirCadena("introduce la contraseña:");
 						
+						int opcionCargo;
 						
 						while (!cargoValido) {
-							String cargoTexto = Utilidades.introducirCadena("Introduce el cargo (monitor / recepcionista): "); 
-							if (cargoTexto.equalsIgnoreCase("monitor")) { 
-								cargo = Cargo.monitor;
-								cargoValido = true;
-							} else if (cargoTexto.equalsIgnoreCase("recepcionista")) { 
-								cargo = Cargo.recepcionista; 
-								cargoValido = true;
+							opcionCargo = Utilidades.leerInt(1, 2, "Introduce el cargo (1-Monitor / 2-Recepcionista):");
+
+						    if (opcionCargo == 1) {
+						        cargo = Cargo.monitor; // El valor del Enum
+						        cargoValido = true;
+						    } else if (opcionCargo == 2) {
+						        cargo = Cargo.recepcionista; // El valor del Enum
+						        cargoValido = true;
 							} else { 
 								System.out.println("Cargo no válido. Debe ser 'monitor' o 'recepcionista'."); 
 							}
@@ -136,12 +143,69 @@ public class Main {
 
 	
 
-	public static void verTrabajadores() {
-	}
+		public static void verTrabajadores(File refFichero) {
+		    if (!refFichero.exists()) {
+		        System.out.println("Fichero no existente");
+		        return;
+		    }
+
+		    try (ObjectInputStream personaIStream = new ObjectInputStream(new FileInputStream(refFichero))) {
+		        System.out.println("--- TRABAJADORES REGISTRADOS ---");
+		        
+		        while (true) {
+		           
+		            Persona p = (Persona) personaIStream.readObject();
+		            
+		            
+		            if (p instanceof Trabajador) {
+		                p.visualizar(); 
+		               
+		            }
+		        }
+		    } catch (EOFException eof) {
+		       
+		    } catch (Exception e) {
+		        System.out.println("Error de lectura: " + e.getMessage());
+		    }
+		}
 	
 
-	public static void verClientesOrdenados() {
-	}
+		public static void verClientesOrdenados(File refFichero) {
+		    
+		    ArrayList<Cliente> listaClientes = new ArrayList<>();
+
+		    if (!refFichero.exists()) {
+		        System.out.println("No existe el fichero.");
+		        return;
+		    }
+
+		  
+		    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(refFichero))) {
+		        while (true) {
+		            Object obj = ois.readObject();
+		            if (obj instanceof Cliente) {
+		                listaClientes.add((Cliente) obj);
+		            }
+		        }
+		    } catch (EOFException e) {
+		       
+		    } catch (Exception e) {
+		        System.out.println("Error al leer: " + e.getMessage());
+		    }
+
+		   
+		    listaClientes.sort((c1, c2) -> c1.getNombre().compareToIgnoreCase(c2.getNombre()));
+
+		   
+		    System.out.println("--- CLIENTES ORDENADOS POR NOMBRE ---");
+		    if (listaClientes.isEmpty()) {
+		        System.out.println("No hay clientes registrados.");
+		    } else {
+		        for (Cliente c : listaClientes) {
+		            c.visualizar(); 
+		        }
+		    }
+		}
 	
 	public static void aniadirEntrenamientoCliente() {
 		
