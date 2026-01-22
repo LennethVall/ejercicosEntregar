06fa8af98f542126c3a748fa5f0d3324aae46663
LennethVall <sindelenia@gmail.com>
2026-01-22 19:32:13 +0100

    Implement Entrenamiento class with properties and methods

--- gimnasioTartanga/clases/Entrenamiento.java
+++ gimnasioTartanga/clases/Entrenamiento.java
@@ -1,6 +1,57 @@
 package clases;
 
-public class Entrenamiento {
-	
 
+
+import java.time.LocalDate;
+
+public class Entrenamiento {
+
+    private String codigo;        // WOD-XXX
+    private LocalDate fecha;      // Fecha del entrenamiento
+    private String ejercicio;     // Nombre del ejercicio
+    private int repeticiones;     // Número de repeticiones
+
+    public Entrenamiento(String codigo, LocalDate fecha, String ejercicio, int repeticiones) {
+        this.codigo = codigo;
+        this.fecha = fecha;
+        this.ejercicio = ejercicio;
+        this.repeticiones = repeticiones;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getEjercicio() {
+        return ejercicio;
+    }
+
+    public void setEjercicio(String ejercicio) {
+        this.ejercicio = ejercicio;
+    }
+
+    public int getRepeticiones() {
+        return repeticiones;
+    }
+
+    public void setRepeticiones(int repeticiones) {
+        this.repeticiones = repeticiones;
+    }
+
+    @Override
+    public String toString() {
+        return codigo + " - " + fecha + " - " + ejercicio + " (" + repeticiones + " reps)";
+    }
 }
